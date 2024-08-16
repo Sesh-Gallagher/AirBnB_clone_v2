48fa81b27bbd002893e5c94242da90bbc74008ca
Sesh-Gallagher <t.ntwana@gmail.com>
2024-08-16 05:52:50 -0700

    3-deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """
-Represents Fabric script based on the file 2-do_deploy_web_static.py that creates and
-distributes an archive to the web servers
+Represents Fabric script based on the file 2-do_deploy_web_static.py
+that creates and distributes an archive to the web servers
 
-execute: fab -f 3-deploy_web_static.py deploy -i ~/.ssh/id_rsa -u ubuntu
-"""
+execute: fab -f 3-deploy_web_static.py deploy -i ~/.ssh/id_rsa -u ubuntu"""
 
 from fabric.api import env, local, put, run
 from datetime import datetime
@@ -21,7 +20,7 @@ def do_pack():
         file_name = "versions/web_static_{}.tgz".format(date)
         local("tar -cvzf {} web_static".format(file_name))
         return file_name
-    except:
+    except Exception:
         return None
 
 
@@ -42,7 +41,7 @@ def do_deploy(archive_path):
         run('rm -rf /data/web_static/current')
         run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
-    except:
+    except Exception:
         return False
 
 
