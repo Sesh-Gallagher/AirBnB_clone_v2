8025e1751d8662ab2baccf46988d15e0a2b7dbb2
Sesh-Gallagher <t.ntwana@gmail.com>
2024-08-16 05:37:19 -0700

    Deploy archive!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,16 +1,32 @@
 #!/usr/bin/python3
 """
-Represents fabric script based on the file 1-pack_web_static.py that distributes an
-archive to the web servers
+Represents Fabric script based on the file 2-do_deploy_web_static.py that creates and
+distributes an archive to the web servers
+
+execute: fab -f 3-deploy_web_static.py deploy -i ~/.ssh/id_rsa -u ubuntu
 """
 
-from os.path import exists
+from fabric.api import env, local, put, run
+from datetime import datetime
+from os.path import exists, isdir
 env.hosts = ['54.208.52.184', '34.227.90.100']
-from fabric.api import put, run, env
+
+
+def do_pack():
+    """ Module to generates a tgz archive"""
+    try:
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        if isdir("versions") is False:
+            local("mkdir versions")
+        file_name = "versions/web_static_{}.tgz".format(date)
+        local("tar -cvzf {} web_static".format(file_name))
+        return file_name
+    except:
+        return None
 
 
 def do_deploy(archive_path):
-    """distributes an archive to the web servers"""
+    """Module distributes an archive to the web servers"""
     if exists(archive_path) is False:
         return False
     try:
@@ -28,3 +44,11 @@ def do_deploy(archive_path):
         return True
     except:
         return False
+
+
+def deploy():
+    """Module creates and distributes an archive to the web servers"""
+    archive_path = do_pack()
+    if archive_path is None:
+        return False
+    return do_deploy(archive_path)
